6ad81cccfb5fde3bb4f7902eb2b13746563227bb
conor-mcnally <cmcn@gmx.co.uk>
2020-10-30 00:48:19 +0000

    images

--- game.py
+++ game.py
@@ -1,1 +1,80 @@
-import pygame
+import pygame, sys
+from pygame.locals import *
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Pandemic Survival")
+clock = pygame.time.Clock()
+screenWidth = 500
+walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.jpg')
+char = pygame.image.load('standing.png')
+
+class player(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 10
+        self.left = False
+        self.right = False
+        self.up = False
+        self.down = False
+        self.walkCount = 0
+
+    def draw(self, win):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if self.left:
+            win.blit(walkLeft[self.walkCount//3], (self.x, self.y))
+            self.walkCount += 1
+        elif self.right:
+            win.blit(walkRight[self.walkCount//3], (self.x, self.y))
+            self.walkCount += 1
+        else:
+            win.blit(char, (self.x, self.y))
+
+def redrawGameWindow():
+    win.blit(bg, (0,0))
+    man.draw(win)
+    pygame.display.update()
+
+#Mainloop
+man = player(200, 410, 64, 64)
+
+while True:
+    #FPS
+    clock.tick(27)
+
+    #Quit function
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+
+    #Controls
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and man.x > man.vel:
+        man.x -= man.vel
+        man.left = True
+        man.right = False
+    elif keys[pygame.K_RIGHT] and man.x < screenWidth - man.width - man.vel:
+        man.x += man.vel
+        man.right = True
+        man.left = False
+    elif keys[pygame.K_UP] and man.y > man.vel:
+        man.y -= man.vel
+        man.up = True
+        man.down = False
+    if keys[pygame.K_DOWN] and man.y < screenWidth - man.height - man.vel:
+        man.y += man.vel
+        man.up = False
+        man.down = True
+
+
+    #Draw shape
+    redrawGameWindow()
